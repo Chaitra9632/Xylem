4f18d8160547dd983739a0e6927bca77e371ffb1
Chaitra <chaitra.sreenivas88@gmail.com>
2021-04-22 17:33:49 +0530

    push

---
+++ SampleProject/src/test/java/crm/Xylem/SampleProject/XylemSample.java
@@ -0,0 +1,13 @@
+package crm.Xylem.SampleProject;
+
+import org.testng.annotations.Test;
+
+public class XylemSample {
+	
+	@Test
+	public void sampleTest()
+	{
+		System.out.println("Hello World..!!!");
+	}
+
+}
