ea421689a7978df39bf7751516ef8a3311bae760
Chaitra <chaitra.sreenivas88@gmail.com>
2021-04-22 17:59:55 +0530

    PUSH

--- SampleProject/src/test/java/crm/Xylem/SampleProject/XylemSample.java
+++ SampleProject/src/test/java/crm/Xylem/SampleProject/XylemSample.java
@@ -8,6 +8,13 @@ public class XylemSample {
 	public void sampleTest()
 	{
 		System.out.println("Hello World..!!!");
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String USER = System.getProperty("user");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USER);
 	}
 
 }
